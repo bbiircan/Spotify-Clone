f21b81b7bf7c90acb9e7fbaeab8bfa61f6e92665
bbiircan <bircanbircan1963@gmail.com>
2025-01-08 20:12:25 +0300

    Made updates and improvements to the code.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,7 +6,7 @@ export const Player = () => {
 
     const {track, seekBar, seekBg, playStatus, play, pause,time,previous,next,seekSong} = useContext(PlayerContext)
 
-    return (
+    return track ? (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
             <div className='hidden lg:flex items-center gap-4'>
                 <img className='w-12' src={track.image} alt="" />
@@ -47,7 +47,7 @@ export const Player = () => {
                 <img className='w-4 cursor-pointer' src={assets.zoom_icon} alt="" />
             </div>
         </div>
-    )
+    ) : null
 }
 
 export default Player
